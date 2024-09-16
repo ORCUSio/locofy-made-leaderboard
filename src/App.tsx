bae3f906218fb76773790c0d11c17c6b6756face
Piyush Rawat <kusum.aayushi@gmail.com>
2024-09-16 22:54:46 +0530

    locofy errors fixed

--- src/App.tsx
+++ src/App.tsx
@@ -8,41 +8,6 @@ import {
 import Leaderboard from "./pages/Leaderboard";
 
 function App() {
-  const action = useNavigationType();
-  const location = useLocation();
-  const pathname = location.pathname;
-
-  useEffect(() => {
-    if (action !== "POP") {
-      window.scrollTo(0, 0);
-    }
-  }, [action, pathname]);
-
-  useEffect(() => {
-    let title = "";
-    let metaDescription = "";
-
-    switch (pathname) {
-      case "/":
-        title = "";
-        metaDescription = "";
-        break;
-    }
-
-    if (title) {
-      document.title = title;
-    }
-
-    if (metaDescription) {
-      const metaDescriptionTag: HTMLMetaElement | null = document.querySelector(
-        'head > meta[name="description"]'
-      );
-      if (metaDescriptionTag) {
-        metaDescriptionTag.content = metaDescription;
-      }
-    }
-  }, [pathname]);
-
   return (
     <Routes>
       <Route path="/" element={<Leaderboard />} />
